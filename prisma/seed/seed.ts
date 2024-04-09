c4c19c6e54de79cf0ab0412227b56238144e863e
Leonardo Matos <leonardolucas2020@gmail.com>
2024-04-08 21:31:55 -0400

    Import article data from json file.

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,41 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 
+import articles from "../../src/data/articles.json";
+import { slugify } from "../../src/helpers/slugify";
+
 const prisma = new PrismaClient();
+
+const isDev = process.env.NODE_ENV === "development";
 
 async function main() {
   console.log("DB Seed!!");
+  await seedArticles();
+}
+
+async function seedArticles() {
+  if (!isDev) {
+    throw new Error("NODE_ENV is not a development environment");
+  }
+
+  await prisma.article.deleteMany();
+  await prisma.$executeRawUnsafe(
+    "DELETE FROM SQLITE_SEQUENCE WHERE name=$1",
+    "Article"
+  );
+
+  for (let article of articles) {
+    const record = await prisma.article.create({
+      data: {
+        title: article.title,
+        slug: slugify(article.title),
+        excerpt: article.excerpt,
+        content: article.content,
+        image: article.image,
+        publishedAt: new Date(article.publish_date),
+      },
+    });
+    console.log("🚀 ~ seedArticles ~ record :", record.id, record.title);
+  }
 }
 
 main()
