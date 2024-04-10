4120426f9928066b8befd218cd49cf0644ed614d
Leonardo Matos <leonardolucas2020@gmail.com>
2024-04-09 20:39:54 -0400

    Services to get Home Articles

--- src/libs/database/Articles.ts
+++ src/libs/database/Articles.ts
@@ -19,6 +19,13 @@ const Article = {
 
     return records;
   },
+  count: async ({ where = {} }) => {
+    const count = await prisma.article.count({
+      where,
+    });
+
+    return count;
+  },
 };
 
 export default Article;
