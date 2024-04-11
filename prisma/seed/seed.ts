fb86099bd8e0573bf3d79ee788705cfb1f660d2f
Leonardo Matos <leonardolucas2020@gmail.com>
2024-04-10 20:21:08 -0400

    Seeding and truncating games and genres

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,6 +1,7 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 
 import articles from "../../src/data/articles.json";
+import games from "../../src/data/games.json";
 import { slugify } from "../../src/helpers/slugify";
 
 const prisma = new PrismaClient();
@@ -9,20 +10,29 @@ const isDev = process.env.NODE_ENV === "development";
 
 async function main() {
   console.log("DB Seed!!");
-  await seedArticles();
-}
-
-async function seedArticles() {
   if (!isDev) {
     throw new Error("NODE_ENV is not a development environment");
   }
 
-  await prisma.article.deleteMany();
-  await prisma.$executeRawUnsafe(
-    "DELETE FROM SQLITE_SEQUENCE WHERE name=$1",
-    "Article"
-  );
+  const [, , ...args] = process.argv;
 
+  const truncate = !!args.find((arg) => arg === "truncate");
+  const articles = !!args.find((arg) => arg === "articles");
+  const games = !!args.find((arg) => arg === "games");
+
+  if (truncate) {
+    if (articles) await truncateArticles();
+    if (games) await truncateGamesAndGenres();
+  }
+
+  if (articles) await seedArticles();
+
+  if (games) await seedGamesAndGenres();
+
+  //await seedGamesAndGenre();
+}
+
+async function seedArticles() {
   for (let article of articles) {
     const record = await prisma.article.create({
       data: {
@@ -36,6 +46,65 @@ async function seedArticles() {
     });
     console.log("🚀 ~ seedArticles ~ record :", record.id, record.title);
   }
+}
+
+async function seedGamesAndGenres() {
+  for (let game of games) {
+    const genres = game.genre.map((title) => {
+      const slug = slugify(title);
+      return {
+        genre: {
+          connectOrCreate: {
+            where: { slug },
+            create: { title, slug },
+          },
+        },
+      };
+    });
+
+    const record = await prisma.games.create({
+      data: {
+        title: game.title,
+        slug: game.slug,
+        year: game.year,
+        image: game.fileName,
+        link: game.link || "#",
+        platform: "Nintendo 64",
+        genres: {
+          create: genres,
+        },
+      },
+    });
+    console.log("🚀 ~ Created game ~ record:", record.id, record.title);
+  }
+}
+
+async function truncateArticles() {
+  console.log("Truncating articles");
+  await prisma.article.deleteMany();
+  await prisma.$executeRawUnsafe(
+    "DELETE FROM SQLITE_SEQUENCE WHERE name=$1",
+    "Article"
+  );
+}
+
+async function truncateGamesAndGenres() {
+  console.log("Truncating games and genres");
+  await prisma.gameGenre.deleteMany();
+  await prisma.games.deleteMany();
+  await prisma.genres.deleteMany();
+  await prisma.$executeRawUnsafe(
+    "DELETE FROM SQLITE_SEQUENCE WHERE name=$1",
+    "GameGenre"
+  );
+  await prisma.$executeRawUnsafe(
+    "DELETE FROM SQLITE_SEQUENCE WHERE name=$1",
+    "Games"
+  );
+  await prisma.$executeRawUnsafe(
+    "DELETE FROM SQLITE_SEQUENCE WHERE name=$1",
+    "Genres"
+  );
 }
 
 main()
