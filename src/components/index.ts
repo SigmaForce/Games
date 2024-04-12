4a84100da0d3433310fe59241236f645a60474ef
Leonardo Matos <leonardolucas2020@gmail.com>
2024-04-11 22:00:19 -0400

    Home page hero

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from "./Icons/Icons";
 
 export { Navbar } from "./Navbar/Navbar";
 export { Pagination } from "./Pagination/Pagination";
+export { PageWrapper } from "./PageWrapper/PageWrapper";
+export { Hero } from "./Hero/Hero";
--- src/services/Games.ts
+++ src/services/Games.ts
@@ -1,3 +1,4 @@
+import { metadata } from "@/app/layout";
 import Games from "@/libs/database/Games";
 
 const GamesService = {
@@ -10,6 +11,25 @@ const GamesService = {
       data,
       metadata: {
         page,
+        limit,
+        offset,
+        total,
+        totalPages,
+      },
+    };
+  },
+
+  getRandomGames: async (limit = 10) => {
+    const total = await Games.count({});
+    const totalPages = Math.ceil(total / limit);
+    const offset = Math.max(0, Math.floor(Math.random() * total) - limit);
+    const data = await Games.get({ limit, offset });
+    const sorted = data.sort(() => (Math.random() > 0.5 ? 1 : -1));
+
+    return {
+      data: sorted,
+      metadata: {
+        page: 1,
         limit,
         offset,
         total,
