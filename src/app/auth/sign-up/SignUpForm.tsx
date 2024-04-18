91f3605245a69f38cbbb26860ca316698dd54009
Leonardo Matos <leonardolucas2020@gmail.com>
2024-04-17 21:19:54 -0400

    server side validation

--- src/app/auth/sign-up/SignUpForm.tsx
+++ src/app/auth/sign-up/SignUpForm.tsx
@@ -2,20 +2,37 @@
 
 import { TextInput, SubmitButton } from "@/components";
 import { useFormState } from "react-dom";
-import { handleSignUpForm } from "./actions";
+import { SignUpState, handleSignUpForm } from "./actions";
+
+const initialState: SignUpState = {
+  isValid: undefined,
+  errors: {
+    name: undefined,
+    email: undefined,
+    password: undefined,
+    passwordConfirmation: undefined,
+  },
+};
 
 export default function SignUpForm() {
-  const [state, formAction] = useFormState(handleSignUpForm, {});
+  const [state, formAction] = useFormState(handleSignUpForm, initialState);
+  console.log("🚀 ~ SignUpForm ~ state:", state);
 
   return (
     <form action={formAction}>
-      <TextInput name="name" label="Name" />
-      <TextInput name="email" label="E-mail" inputMode="email" />
-      <TextInput name="password" label="Password" type="password" />
+      <TextInput name="name" label="Name" error={state.errors.name} />
+      <TextInput name="email" label="E-mail" error={state.errors.email} />
+      <TextInput
+        name="password"
+        label="Password"
+        type="password"
+        error={state.errors.password}
+      />
       <TextInput
         name="PasswordConfirmation"
         label="Password confirmation"
         type="password"
+        error={state.errors.passwordConfirmation}
       />
 
       <SubmitButton>Create Account!</SubmitButton>
